dbd40133617f8b50b15497c20e23e1c9a8fdf8e0
HrayrMuradyan <82998878+HrayrMuradyan@users.noreply.github.com>
2025-02-28 21:07:54 -0800

    Feature/dashboard structure and two plots (#26)
    * added filters and cards
    
    * Adjusted the app structure, added two (not final) plots
    
    ---------
    
    Co-authored-by: Mingyang Zhang <mingyang.zhangmz@gmail.com>

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from dash import Input, Output
+from dash import Input, Output, html
 from app import app
+import altair as alt
+alt.data_transformers.enable('default', max_rows=50000)
 
 df = pd.read_csv("data/processed/prep_bank_marketing.csv")
 
@@ -10,20 +12,70 @@ df['y_numeric'] = df['y'].map({'yes': 1, 'no': 0})
 # Standardize job categories
 df['job_prep'] = df['job_prep'].str.lower().str.strip()
 
+plot_width = 380
+plot_height = 190
+
+def create_balance_plot(data):
+    chart = alt.Chart(data, width='container').transform_density(
+        'balance', as_=['balance', 'density'], groupby=['y']
+    ).mark_area(opacity=0.5).encode(
+        x=alt.X('balance:Q', title='Balance', axis=alt.Axis(titleFontSize=16)),
+        y=alt.Y('density:Q', title='Density', axis=alt.Axis(titleFontSize=16)),
+        color=alt.Color('y:N', scale=alt.Scale(domain=['yes', 'no'], range=['green', '#d84e5f']))
+    ).properties(
+        width=plot_width,  
+        height=plot_height
+    ).interactive()
+
+    return chart.to_dict()
+
+def create_contact_plot(data):
+    chart = alt.Chart(data, width='container').transform_density(
+        'campaign', as_=['campaign', 'density'], groupby=['y']
+    ).mark_area(opacity=0.5).encode(
+        x=alt.X('campaign:Q', title='Capaign', axis=alt.Axis(titleFontSize=16)),
+        y=alt.Y('density:Q', title='Density', axis=alt.Axis(titleFontSize=16)),
+        color=alt.Color('y:N', scale=alt.Scale(domain=['yes', 'no'], range=['green', '#d84e5f']))
+    ).properties(
+        width=plot_width,  
+        height=plot_height
+    ).interactive()
+
+    return chart.to_dict()
+
+def return_empty(balance_plot_spec, contact_plot_spec):
+    subscribed_summary = [
+                html.P(f"Yes: 0"),
+                html.P(f"No: 0")
+            ]
+    return (
+        0,
+        0,
+        0,
+        0,
+        subscribed_summary,
+        balance_plot_spec,
+        contact_plot_spec
+    )
+  
 @app.callback(
     [Output("prop_subscribed", "children"),
      Output("avg_contacts_campaign", "children"),
      Output("avg_contacts_before", "children"),
      Output("avg_last_contact", "children"),
      Output("subscribed_summary", "children")],
+     Output('balance_plot', 'spec'),
+     Output('contact_plot', 'spec'),
     [Input("year_filter", "value"),
      Input("age_filter", "value"),
      Input("marital_filter", "value"),
      Input("job_filter", "value")],
 )
+
+
 def update_cards(selected_years, selected_age, selected_marital, selected_job):
-    if not selected_years or not selected_marital or selected_job is None:
-        return ["N/A"] * 5
+    # if not selected_years or not selected_marital or selected_job is None:
+    #     return return_empty()
 
     min_age, max_age = selected_age
 
@@ -34,20 +86,31 @@ def update_cards(selected_years, selected_age, selected_marital, selected_job):
         (df["job_prep"] == selected_job.lower() if selected_job else False)
     ]
 
+    balance_plot = create_balance_plot(filtered_df)
+    contact_plot = create_contact_plot(filtered_df)
+
     if filtered_df.empty:
-        return ["N/A"] * 5
+        return return_empty(balance_plot, contact_plot)
 
     prop_subscribed = filtered_df["y_numeric"].mean()
     avg_contacts_campaign = filtered_df["campaign"].mean()
     avg_contacts_before = filtered_df["previous"].mean()
     avg_last_contact = filtered_df["duration"].mean()
 
-    subscribed_summary = f"Yes: {sum(filtered_df['y_numeric'])} / No: {len(filtered_df) - sum(filtered_df['y_numeric'])}"
+    yes_count = sum(filtered_df['y_numeric'])
+    no_count = len(filtered_df) - yes_count
+
+    subscribed_summary = [
+        html.P(f"Yes: {yes_count}", style={'margin': '0px'}),
+        html.P(f"No: {no_count}", style={'margin': '0px'})
+    ]
 
     return (
         f"{prop_subscribed:.2f}",
         f"{avg_contacts_campaign:.2f}",
         f"{avg_contacts_before:.2f}",
-        f"{avg_last_contact:.1f} mins",
-        subscribed_summary
+        f"{avg_last_contact:.1f}",
+        subscribed_summary,
+        balance_plot,
+        contact_plot
     )
